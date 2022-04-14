343b7b8373e6cd2572e526c4e95ef4577bba7bc3
ret2me <filip.poplewski@gmail.com>
2022-04-14 19:18:55 +0200

    Automate database creation.

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -7,8 +7,10 @@ import config
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 # Import scanner engine class
+import scanner
 from scanner.scanner import WebsiteCrawler
 
 # Load extensions
@@ -29,6 +31,10 @@ WebsiteCrawler.svn = git
 
 # Init database
 engine = create_engine(config.DATABASE_URL, echo=True)
+if not database_exists(config.DATABASE_URL):
+    create_database(config.DATABASE_URL)
+else:
+    engine.connect()
 Base = declarative_base()
 
 # Import all tables
@@ -39,3 +45,38 @@ Base.metadata.create_all(engine)
 # Create engine handler
 Session = sessionmaker(bind=engine)
 session = Session()
+
+query = session.query(Vulnerabilities).count()
+if query == 0:
+    git_vulnerability = Vulnerabilities(id=1,
+                                        name='GIT exposure',
+                                        severity_level=9,
+                                        endpoint='/.git/',
+                                        description=('When .git folder is also deployed along with the web ',
+                                                     'application, the attacker could exploit this ',
+                                                     'misconfiguration to download the entire source code ',
+                                                     'along with other sensitive ',
+                                                     'data.  More info at:',
+                                                     'https://iosentrix.com/blog/git-source-code-disclosure-vulnerability/'))
+    svn_vulnerability = Vulnerabilities(id=2,
+                                        name='SVN exposure',
+                                        severity_level=9,
+                                        endpoint='/.svn/',
+                                        description=('When .svn folder is also deployed along with the web ',
+                                                     'application, the attacker could exploit this misconfiguration ',
+                                                     'to download the entire source code along with other sensitive ',
+                                                     'data. More info at: ',
+                                                     'https://medium.com/@ghostlulzhacks/exposed-source-code-c16fac0032ffd'))
+
+    env_vulnerability = Vulnerabilities(id=3,
+                                        name='ENV exposure',
+                                        severity_level=9,
+                                        endpoint='/.env/',
+                                        description=('When .env file is also deployed along with the web ',
+                                                     'application, the attacker could exploit this misconfiguration ',
+                                                     'to download the entire environment variables file with ',
+                                                     'sensitive data like database credentials. More info at: ',
+                                                     'https://www.acunetix.com/vulnerabilities/web/dotenv-env-file/'))
+
+    session.add_all([git_vulnerability, svn_vulnerability, env_vulnerability])
+    session.commit()
